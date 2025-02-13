c11d6948f82bbc3b8d75825304635fe8a1493685
Liviu Mazilu <liviumazilu79@gmail.com>
2025-02-13 10:56:55 +0100

    added checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-checkout',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatButtonModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
-export class CheckoutComponent {}
+export class CheckoutComponent {
+  data = inject(MAT_DIALOG_DATA);
+}
--- src/app/guards/checkout.guard.ts
+++ src/app/guards/checkout.guard.ts
@@ -1,14 +1,26 @@
 import { inject } from '@angular/core';
 import { CanDeactivateFn } from '@angular/router';
 import { CartService } from '../services/cart.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CheckoutComponent } from '../checkout/checkout.component';
 
-export const checkoutGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-
+export const checkoutGuard: CanDeactivateFn<unknown> = (
+  component,
+  currentRoute,
+  currentState,
+  nextState
+) => {
   const cartService = inject(CartService);
 
-  if (cartService.cart) { 
-    const confirmation = confirm('Sei sicuro di uscire dal carrello?');
-    return confirmation;
+  if (cartService.cart) {
+    const dialog = inject(MatDialog);
+
+    if (cartService.cart) {
+      const confirmation = dialog
+        .open(CheckoutComponent, { data: cartService.cart.products.length })
+        .afterClosed();
+      return confirmation;
+    }
   }
 
   return true;
