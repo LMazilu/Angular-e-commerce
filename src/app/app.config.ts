a495d71bfb6711daeaccb0ac12e18e72c37d15fb
Liviu Mazilu <liviumazilu79@gmail.com>
2025-02-13 13:04:39 +0100

    added ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,7 @@ import { routes } from './app.routes';
 import { APP_SETTINGS, appSettings } from './app.settings';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +20,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor])),
     { provide: APP_SETTINGS, useValue: appSettings },
     { provide: ErrorHandler, useClass: AppErrorHandler },
-    provideAnimationsAsync(),
+    provideAnimationsAsync(), provideClientHydration(withEventReplay()),
   ],
 };
