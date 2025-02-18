41d27212d34f46d89ea484c44b39286bdc92cb38
Liviu Mazilu <liviumazilu79@gmail.com>
2025-02-18 09:42:36 +0100

    Optimized ssr performance

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,21 +5,29 @@ import {
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { AppErrorHandler } from './app-error-handler';
 import { routes } from './app.routes';
 import { APP_SETTINGS, appSettings } from './app.settings';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     { provide: APP_SETTINGS, useValue: appSettings },
     { provide: ErrorHandler, useClass: AppErrorHandler },
-    provideAnimationsAsync(), provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideClientHydration(withEventReplay()),
   ],
 };
